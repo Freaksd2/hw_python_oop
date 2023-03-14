99ab664e3a73baba9f6abb8d62a7a44b209f9502
Freaksd2 <117035956+Freaksd2@users.noreply.github.com>
2023-03-15 01:29:55 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -104,7 +104,7 @@ class SportsWalking(Training):
         return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight
                  + ((self.get_mean_speed()
                      * self.KMH_IN_MSEC) ** 2
-                 / (self.height / self.CM_IN_M))
+                    / (self.height / self.CM_IN_M))
                 * self.CALORIES_SPEED_HEIGHT_MULTIPLIER
                 * self.weight)
                 * (self.duration * self.MIN_IN_H))
